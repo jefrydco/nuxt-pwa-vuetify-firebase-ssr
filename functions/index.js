7a92ac18a3ea4a436378562be26512cceee651b2
Jefry Dewangga <jefrydco@gmail.com>
2020-04-25 13:57:53 +0700

    style(functions): eslint

--- functions/index.js
+++ functions/index.js
@@ -4,30 +4,30 @@
  *
  * Credit: https://github.com/davidroyer/nuxt-ssr-firebase
  */
-const functions = require("firebase-functions");
-const { Nuxt } = require("nuxt");
-const express = require("express");
+const functions = require('firebase-functions')
+const { Nuxt } = require('nuxt')
+const express = require('express')
 
-const app = express();
+const app = express()
 
 const config = {
   /**
    * Development mode
    */
   dev: false
-};
-
-const nuxt = new Nuxt(config);
-
-function handleRequest(req, res) {
-  res.set("Cache-Control", "public, max-age=600, s-maxage=1200");
-  return new Promise((resolve, reject) => {
-    nuxt.render(req, res, promise => {
-      promise.then(resolve).catch(reject);
-    });
-  });
 }
 
-app.use(handleRequest);
+const nuxt = new Nuxt(config)
 
-exports.nuxtssr = functions.https.onRequest(app);
+function handleRequest(req, res) {
+  res.set('Cache-Control', 'public, max-age=600, s-maxage=1200')
+  return new Promise((resolve, reject) => {
+    nuxt.render(req, res, (promise) => {
+      promise.then(resolve).catch(reject)
+    })
+  })
+}
+
+app.use(handleRequest)
+
+exports.nuxtssr = functions.https.onRequest(app)
