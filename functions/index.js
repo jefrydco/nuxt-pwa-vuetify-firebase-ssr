6ce62b601fd8112e8f700888865f0ac4923286f4
Jefry Dewangga <jefrydco@gmail.com>
2020-04-25 14:39:52 +0700

    refactor(functions): wait nuxt to be ready

--- functions/index.js
+++ functions/index.js
@@ -19,13 +19,23 @@ const config = {
 
 const nuxt = new Nuxt(config)
 
-function handleRequest(req, res) {
-  res.set('Cache-Control', 'public, max-age=600, s-maxage=1200')
-  return new Promise((resolve, reject) => {
-    nuxt.render(req, res, (promise) => {
-      promise.then(resolve).catch(reject)
-    })
+// Taken from: https://github.com/nuxt/nuxt.js/issues/5377#issuecomment-479371915
+let isReady = false
+const readyPromise = nuxt
+  .ready()
+  .then(() => {
+    isReady = true
   })
+  .catch(() => {
+    process.exit(1)
+  })
+
+async function handleRequest(req, res) {
+  if (!isReady) {
+    await readyPromise
+  }
+  res.set('Cache-Control', 'public, max-age=600, s-maxage=1200')
+  await nuxt.render(req, res)
 }
 
 app.use(handleRequest)
